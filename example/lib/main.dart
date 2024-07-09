8a4496d5f475e4035f748f057531ce0694f8c828
Matthew Rawson <mrawson@redev.co.uk>
2024-07-09 10:03:37 +0100

    Added example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 import 'package:windows_audio/windows_audio.dart';
 
@@ -31,8 +30,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _windowsAudioPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _windowsAudioPlugin.getPlatformVersion() ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -55,7 +54,20 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Running on: $_platformVersion\n'),
+              ElevatedButton(
+                onPressed: () async {
+                  // Load and play an audio file
+                  await _windowsAudioPlugin.load("path/to/audio");
+                  _windowsAudioPlugin.play();
+                },
+                child: const Text('Play Audio'),
+              ),
+            ],
+          ),
         ),
       ),
     );
