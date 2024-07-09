dddc7d212e827dadfe0e7d7a434aa7f9e0e747ff
Matthew Rawson <mrawson@redev.co.uk>
2024-07-09 10:03:48 +0100

    Added example project

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <windows_audio/windows_audio_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WindowsAudioPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowsAudioPluginCApi"));
+}
